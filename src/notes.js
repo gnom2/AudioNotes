b8e66e5907484943f56c59fe275d1a7c599dd6a8
gnomee <heavensarch@gmail.com>
2020-04-28 17:21:31 -0400

    Add styling

--- src/notes.js
+++ src/notes.js
@@ -26,26 +26,25 @@ const d4 = document.querySelectorAll('.checkbox-row23');
 const dSh4 = document.querySelectorAll('.checkbox-row22');
 const e4 = document.querySelectorAll('.checkbox-row21');
 const f4 = document.querySelectorAll('.checkbox-row20');
-const fSh4 = document.querySelectorAll('.checkbox-row23');
-const g4 = document.querySelectorAll('.checkbox-row22');
-const gSh4 = document.querySelectorAll('.checkbox-row21');
-const a4 = document.querySelectorAll('.checkbox-row20');
-const aSh4 = document.querySelectorAll('.checkbox-row20');
-const b4 = document.querySelectorAll('.checkbox-row20');
-const c5 = document.querySelectorAll('.checkbox-row20');
-const cSh5 = document.querySelectorAll('.checkbox-row20');
-const d5 = document.querySelectorAll('.checkbox-row20');
-const dSh5 = document.querySelectorAll('.checkbox-row20');
-const e5 = document.querySelectorAll('.checkbox-row20');
-const f5 = document.querySelectorAll('.checkbox-row20');
-const e4 = document.querySelectorAll('.checkbox-row21');
-const f4 = document.querySelectorAll('.checkbox-row20');
-const fSh4 = document.querySelectorAll('.checkbox-row23');
-const g4 = document.querySelectorAll('.checkbox-row22');
-const gSh4 = document.querySelectorAll('.checkbox-row21');
-const a4 = document.querySelectorAll('.checkbox-row20');
-const aSh4 = document.querySelectorAll('.checkbox-row20');
-const b4 = document.querySelectorAll('.checkbox-row20');
+const fSh4 = document.querySelectorAll('.checkbox-row19');
+const g4 = document.querySelectorAll('.checkbox-row18');
+const gSh4 = document.querySelectorAll('.checkbox-row17');
+const a4 = document.querySelectorAll('.checkbox-row16');
+const aSh4 = document.querySelectorAll('.checkbox-row15');
+const b4 = document.querySelectorAll('.checkbox-row14');
+const c5 = document.querySelectorAll('.checkbox-row13');
+const cSh5 = document.querySelectorAll('.checkbox-row12');
+const d5 = document.querySelectorAll('.checkbox-row11');
+const dSh5 = document.querySelectorAll('.checkbox-row10');
+const e5 = document.querySelectorAll('.checkbox-row9');
+const f5 = document.querySelectorAll('.checkbox-row8');
+const fSh5 = document.querySelectorAll('.checkbox-row7');
+const g5 = document.querySelectorAll('.checkbox-row6');
+const gSh5 = document.querySelectorAll('.checkbox-row5');
+const a5 = document.querySelectorAll('.checkbox-row4');
+const aSh5 = document.querySelectorAll('.checkbox-row3');
+const b5 = document.querySelectorAll('.checkbox-row2');
+const c6 = document.querySelectorAll('.checkbox-row1');
 
 for (let i = 0; i < c4.length; i++) {
     c4[i].addEventListener('click', async () => {
@@ -58,5 +57,167 @@ for (let i = 0; i < cSh4.length; i++) {
     cSh4[i].addEventListener('click', async () => {
         await ToneNote.start();
         synthNote.triggerAttackRelease('C#4', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < d4.length; i++) {
+    d4[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('D4', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < dSh4.length; i++) {
+    dSh4[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('D#4', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < e4.length; i++) {
+    e4[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('E4', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < f4.length; i++) {
+    f4[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('F4', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < fSh4.length; i++) {
+    fSh4[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('F#4', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < g4.length; i++) {
+    g4[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('G4', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < gSh4.length; i++) {
+    gSh4[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('G#4', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < a4.length; i++) {
+    a4[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('A4', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < aSh4.length; i++) {
+    aSh4[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('A#4', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < b4.length; i++) {
+    b4[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('B4', '8n').volume.value = 3;
+    });
+}
+
+
+for (let i = 0; i < c5.length; i++) {
+    c5[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('C5', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < cSh5.length; i++) {
+    cSh5[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('C#5', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < d5.length; i++) {
+    d5[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('D5', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < dSh5.length; i++) {
+    dSh5[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('D#5', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < e5.length; i++) {
+    e5[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('E5', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < f5.length; i++) {
+    f5[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('F5', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < fSh5.length; i++) {
+    fSh5[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('F#5', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < g5.length; i++) {
+    g5[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('G5', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < gSh5.length; i++) {
+    gSh5[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('G#5', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < a5.length; i++) {
+    a5[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('A5', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < aSh5.length; i++) {
+    aSh5[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('A#5', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < b5.length; i++) {
+    b5[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('B5', '8n').volume.value = 3;
+    });
+}
+
+for (let i = 0; i < c6.length; i++) {
+    c6[i].addEventListener('click', async () => {
+        await ToneNote.start();
+        synthNote.triggerAttackRelease('C6', '8n').volume.value = 3;
     });
 }
--- src/synth.js
+++ src/synth.js
@@ -29,7 +29,7 @@ const pause = document.body.querySelector('.pause-btn');
 const rows = document.body.querySelectorAll('.note-container > div');
 const tempo = document.body.querySelector('.tempo-control')
 // const notes = ['C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5']
-const notes = ['B5', 'A#5', 'A5', 'G#5', 'G5', 'F#5', 'F5', 'E5', 'D#5', 'D5', 'C#5', 'C5', 'B4', 'A#4', 'A4', 'G#4', 'G4', 'F#4', 'F4', 'E4', 'D#4', 'D4', 'C#4', 'C4']
+const notes = ['C6', 'B5', 'A#5', 'A5', 'G#5', 'G5', 'F#5', 'F5', 'E5', 'D#5', 'D5', 'C#5', 'C5', 'B4', 'A#4', 'A4', 'G#4', 'G4', 'F#4', 'F4', 'E4', 'D#4', 'D4', 'C#4', 'C4']
 Tone.Transport.scheduleRepeat(repeat, '8n');
 Tone.Transport.bpm.value = 128;
 
